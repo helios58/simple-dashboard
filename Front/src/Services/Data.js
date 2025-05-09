abb65ea31138535e55b6c00e24ee993e7427cc21
Mohamed abderrazak <abderrazzak.mohamed@gmail.com>
2025-05-09 17:18:11 +0100

    add react env

--- Front/src/Services/Data.js
+++ Front/src/Services/Data.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3030';
 export const getCardData = async (id, startTimestamp = null, endTimestamp = null) => {
   try {
-    const response = await axios.get(`http://localhost:3030/data/cardData/${id}`, {
+    const response = await axios.get(`${apiUrl}/data/cardData/${id}`, {
       params: {
         startTimestamp,
         endTimestamp,
@@ -16,7 +16,7 @@ export const getCardData = async (id, startTimestamp = null, endTimestamp = null
 };
 export const getSaleScore = async (id) => {
     try {
-      const response = await axios.get(`http://localhost:3030/data/saleScore/${id}`);
+      const response = await axios.get(`${apiUrl}/data/saleScore/${id}`);
       return response.data.score;
     } catch (error) {
       console.error('Error fetching data:', error);
--- Front/src/Services/Insights.js
+++ Front/src/Services/Insights.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3030';
+
 export const getBestInfluencer = async () => {
     try {
-      const response = await axios.get(`http://localhost:3030/insight/bestInfluencer`);
+      const response = await axios.get(`${apiUrl}/insight/bestInfluencer`);
       return response.data.firstName;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -10,7 +12,7 @@ export const getBestInfluencer = async () => {
 
   export const getBestCountry = async () => {
     try {
-      const response = await axios.get(`http://localhost:3030/insight/getBestCountry`);
+      const response = await axios.get(`${apiUrl}/insight/getBestCountry`);
       return response.data.mostUsedCountry;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -19,7 +21,7 @@ export const getBestInfluencer = async () => {
 
   export const getBestCategory = async () => {
     try {
-      const response = await axios.get(`http://localhost:3030/insight/getBestCategory`);
+      const response = await axios.get(`${apiUrl}/insight/getBestCategory`);
       return response.data.categoryText;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -27,7 +29,7 @@ export const getBestInfluencer = async () => {
   };
   export const getBestDevice = async () => {
     try {
-      const response = await axios.get(`http://localhost:3030/insight/getBestDevice`);
+      const response = await axios.get(`${apiUrl}/insight/getBestDevice`);
       return response.data.bestDevice;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -35,7 +37,7 @@ export const getBestInfluencer = async () => {
   };
   export const getBestColor = async () => {
     try {
-      const response = await axios.get(`http://localhost:3030/insight/getBestColor`);
+      const response = await axios.get(`${apiUrl}/insight/getBestColor`);
       return response.data.colorText;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -44,7 +46,7 @@ export const getBestInfluencer = async () => {
 
   export const getBestBrand = async () => {
     try {
-      const response = await axios.get(`http://localhost:3030/insight/getBestBrand`);
+      const response = await axios.get(`${apiUrl}/insight/getBestBrand`);
       return response.data.mostUsedBrand;
     } catch (error) {
       console.error('Error fetching data:', error);
